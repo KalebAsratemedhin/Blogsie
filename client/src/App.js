f6451d5d89716c94ab620f665bc87b866147d795
KalebAsratemedhin <risekab@gmail.com>
2024-07-14 09:59:32 +0300

    setup client with react

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+
+import {Container, Typography} from '@mui/material'
+import {Favorite} from '@mui/icons-material'
+function App() {
+  return (
+    <Container>
+      <Typography variant='h2'>
+        Hello React
+      </Typography>
+      <Favorite />
+
+    </Container>
+  );
+}
+
+export default App;
